ad2b515674aed00821f70d68c19c569a65c401cf
tanvi <tanvi.goyal@jasperindia.com>
2013-03-11 13:18:53 +0530

    changes in app test for the second time

--- src/test/java/com/snapdeal/GitProject/AppTest.java
+++ src/test/java/com/snapdeal/GitProject/AppTest.java
@@ -1,2 +1,14 @@
 package com.snapdeal.GitProject;
 
+import org.testng.annotations.Test;
+
+public class AppTest{
+	
+	
+	@Test
+	public void demoTest()
+	{
+		System.out.println("This is AppTest Class");
+		System.out.println("modifying first time");
+	}
+}
